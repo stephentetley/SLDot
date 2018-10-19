2cdd17d61e3ef030161ddbc039754872607bd5bc
stephentetley <stephen.tetley@gmail.com>
2018-10-19 21:45:55 +0100

    Adding initial files.

---
+++ test/Test01.fsx
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Stephen Tetley 2018
+// License: BSD 3 Clause
+
+#load "../src/SLDot/DotOutput.fs"
+open SLDot.DotOutput
+
+
+
+let test01 () : unit = 
+    let procM = 
+        graphvizOutput { 
+            do! lineComment "dot -Tjpeg mydot.dot -o mydot.jpg"
+            do! digraph "G1" <| 
+                graphvizOutput { 
+                    do! attrib <| rankdir LR
+                    do! attrib <| ranksep 1.5
+                    do! nodeAttributes [fontname "Arial"; fontsize 10]
+                    do! anonSubgraph 
+                        <| graphvizOutput { 
+                            do! node "node1" []
+                            do! node "node2" []
+                            do! node "node3" []
+                            do! edge "node1" "node2" []
+                            do! edges "node3" ["node2";"node1"] []
+                            return ()
+                            } 
+                }
+            }
+    ignore <| runGraphvizOutputConsole procM
